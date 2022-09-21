ff02896b7b323dc3f47c91894d884a0361a19cf6
Ian Nordby <ian.c.nordby@gmail.com>
2022-09-21 08:55:19 -0600

    Add array methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,13 @@
 var strings = ['hello', 'world', 'turtle', 'tiger'];
 var nums = [12, 23, 34, 45]
 var bool = [true, false, true, true]
+
+// add 'nope!' to end of array
+strings.push('nope!');
+
+// remove and return the last element 45
+nums.pop();
+
+// create a shallow copy of the array from index 0
+// up to but not including index 3 [true, false, true]
+bool.slice(0, 3);
