99fed690014d551f0008a51902174d633e74902b
Ian Nordby <ian.c.nordby@gmail.com>
2022-09-21 09:08:41 -0600

    Add index position practice

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-var strings = ['hello', 'world', 'turtle', 'tiger'];
+var strings = ['hello', 'turtle', 'purple', 'world'];
 var nums = [12, 23, 34, 45]
 var bool = [true, false, true, true]
 
@@ -6,8 +6,17 @@ var bool = [true, false, true, true]
 strings.push('nope!');
 
 // remove and return the last element 45
-nums.pop();
+var last = nums.pop();
+console.log(last)
 
 // create a shallow copy of the array from index 0
 // up to but not including index 3 [true, false, true]
 bool.slice(0, 3);
+
+for (var i = 0; i < nums.length; i++) {
+  console.log(nums[i])
+};
+
+// utilize index positions
+console.log(strings[0] + ' ' + strings[3])
+console.log(`Are ${strings[2]} ${strings[1]}s real?: ${bool[0]}`)
